743cff171d9b2187d2e3e52167d2570225e471c3
Lochini <luhradik1@gmail.com>
2025-07-05 19:03:26 +0530

    add MUI form layout for registration

--- src/views/pages/UserRegistration/UserRegistration.jsx
+++ src/views/pages/UserRegistration/UserRegistration.jsx
@@ -1,9 +1,17 @@
 import Table from "../../components/Table";
 import { useState, useEffect } from "react";
 import { sampleGetAPI } from "../../../api/user";
-import { Box, TextField, InputLabel, Button } from "@mui/material";
+import {
+  Box,
+  TextField,
+  InputLabel,
+  Button,
+  Typography,
+  Grid,
+  Paper,
+} from "@mui/material";
 import { Controller, useForm } from "react-hook-form";
-import "./userRegistration.css";
+// import "./userRegistration.css";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import axios from "axios";
@@ -28,7 +36,10 @@ const UserRegistration = () => {
       email: yup.string().email(),
       nic: yup
         .string()
-        .matches(/^[0-9]{10}$/,"Invalid NIC format. Must be with 12 digits or 12 digits with v or V"),
+        .matches(
+          /^[0-9]{10}$/,
+          "Invalid NIC format. Must be with 12 digits or 12 digits with v or V"
+        ),
     })
     .required();
 
@@ -69,90 +80,173 @@ const UserRegistration = () => {
 
   return (
     <>
-      <h1>User Registration</h1>
+      <Typography variant="h4" gutterBottom>
+        User Registration
+      </Typography>
 
       <Box
         component="form"
-        sx={{ "& .MuiTextField-root": { m: 1, width: "25ch" } }}
+        sx={{
+          // border: 1,
+          // borderColor: "red",
+          justifyContent: "center",
+          // ".inputField": { alignItems:"left", border: 1, marigin:3 },
+          display: "flex",
+        }}
         noValidate
         onSubmit={handleSubmit(onSubmit)}
         autoComplete="off"
       >
-        <div className="firstNameInputField">
-          <InputLabel className="inputLabel">First Name</InputLabel>
-          <Controller
-            name="firstName"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                id="outlined-required"
-                className="inputField"
-                // label="First Name"
-              />
-            )}
-          />
-          {errors.firstName && <p>{errors?.firstName?.message}</p>}
-        </div>
+        <Paper
+          sx={{
+            width: "50%",
+            padding: 5,
+            alignItems: "center",
+          }}
+        >
+          {/* form content  */}
+          <Grid
+            sx={{
+              // border: 1,
+              // borderColor: "greenyellow",
+              margin: 3,
+            }}
+          >
+            {/* first name and last name container */}
+            <Grid
+              sx={{
+                // border: 1,
+                // borderColor: "green",
+                display: "flex",
+                flexDirection: "row",
+                gap: 2,
+              }}
+            >
+              {/* firstName */}
+              <Grid
+                item
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 1,
+                  // border: 1,
+                  // borderColor: "red",
+                  width:"50%"
+                }}
+              >
+                <InputLabel className="inputLabel" sx={{paddingBottom: 3, minWidth: 100 }}>First Name : </InputLabel>
+                <Controller
+                  name="firstName"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      id="outlined-required"
+                      error={!!errors.firstName}
+                      helperText={errors.firstName?.message || " "}
+                      size="small"
+                      className="inputField"
+                    />
+                  )}
+                />
+              </Grid>
 
-        <div className="lastNameInputField">
-          <InputLabel className="inputLabel">Last Name</InputLabel>
-          <Controller
-            name="lastName"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                id="outlined-required"
-                className="inputField"
-                // label="Last Name"
-              />
-            )}
-          />
-          {errors.lastName && <p>{errors?.lastName?.message}</p>}
-        </div>
+              {/* lastName */}
+              <Grid
+                item
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 1,
+                  // border: 1,
+                  // borderColor: "red",
+                  width:"50%"
+                }}
+              >
+                <InputLabel className="inputLabel" sx={{paddingBottom: 3, minWidth: 100 }}>Last Name :</InputLabel>
+                <Controller
+                  name="lastName"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      id="outlined-required"
+                      size="small"
+                      className="inputField"
+                      error={!!errors.lastName}
+                      helperText={errors.lastName?.message || " "}
+                      // fullWidth
+                    />
+                  )}
+                />
+              </Grid>
+            </Grid>
 
-        <div className="emailInputField">
-          <InputLabel className="inputLabel">Email</InputLabel>
-          <Controller
-            name="email"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                id="outlined-required"
-                className="inputField"
-                // label="Email"
+          {/* Email */}
+            <Grid
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: 1,
+                // border: 1,
+                // borderColor: "yellow",
+              }}
+            >
+              <InputLabel className="inputLabel" sx={{paddingBottom: 3, minWidth: 100 }}>Email :</InputLabel>
+              <Controller
+                name="email"
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    id="outlined-required"
+                    size="small"
+                    className="inputField"
+                    error={!!errors.email}
+                    helperText={errors.email?.message || " "}
+                  />
+                )}
               />
-            )}
-          />
-          {errors.email && <p>{errors?.email?.message}</p>}
-        </div>
+            </Grid>
 
-        <div className="nicInputField">
-          <InputLabel className="inputLabel">NIC</InputLabel>
-          <Controller
-            name="nic"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                id="outlined-required"
-                className="inputField"
-                // label="NIC"
+          {/* NIC */}
+            <Grid
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: 1,
+                // border: 1,
+                // borderColor: "yellow",
+              }}
+            >
+              <InputLabel className="inputLabel" sx={{paddingBottom: 3, minWidth: 100 }}>NIC :</InputLabel>
+              <Controller
+                name="nic"
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    id="outlined-required"
+                    size="small"
+                    className="inputField"
+                    sx={{ width: "400px" }}
+                    error={!!errors.nic}
+                    helperText={errors.nic?.message || " "}
+                  />
+                )}
               />
-            )}
-          />
-          {errors.nic && <p>{errors?.nic?.message}</p>}
-        </div>
-
-        <Button type="submit" variant="contained" color="primary">
-          Submit
-        </Button>
-        {submitError && <p>{submitError}</p>}
+            </Grid>
+          
+          {/* Button */}
+            <Button type="submit" variant="contained" color="primary">
+              Submit
+            </Button>
+            {submitError && <p>{submitError}</p>}
+          </Grid>
+        </Paper>
       </Box>
 
-      <Table data={responseData} />
+      {/* <Table data={responseData} /> */}
     </>
   );
 };
