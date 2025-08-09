60b0cbedf9d79469d7be809637554f05c6e647c5
Lochini <luhradik1@gmail.com>
2025-08-09 18:24:59 +0530

    minor changes in theme

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "@emotion/react";
 import "./index.css";
 import App from "./App.jsx";
-import theme from "./theme.js";
+import theme from "./theme";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
